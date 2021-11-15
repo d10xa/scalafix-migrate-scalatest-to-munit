14f1f741cea8384111c67040e7e16182e25a3b6d
d10xa <d10xa@mail.ru>
2021-11-15 23:37:03 +0300

    Remove 'protected' mod from beforeAll and afterAll

--- scalafix/output/src/main/scala/fix/BeforeAndAfter.scala
+++ scalafix/output/src/main/scala/fix/BeforeAndAfter.scala
@@ -3,6 +3,6 @@ package fix
 import munit.FunSuite
 
 abstract class BeforeAndAfter extends FunSuite  {
-  override protected def beforeAll(): Unit = ()
-  override protected def afterAll(): Unit = ()
+  override def beforeAll(): Unit = ()
+  override def afterAll(): Unit = ()
 }
--- scalafix/rules/src/main/scala/fix/ScalafixMigrateScalatestToMunit.scala
+++ scalafix/rules/src/main/scala/fix/ScalafixMigrateScalatestToMunit.scala
@@ -4,6 +4,7 @@ import scalafix.v1._
 
 import scala.meta._
 import scala.meta.tokens.Token
+import scala.meta.contrib.Whitespace
 
 class ScalafixMigrateScalatestToMunit extends SemanticRule("ScalafixMigrateScalatestToMunit") {
 
@@ -18,6 +19,11 @@ class ScalafixMigrateScalatestToMunit extends SemanticRule("ScalafixMigrateScala
 
   override def fix(implicit doc: SemanticDocument): Patch = {
     doc.tree.collect {
+      case d@Defn.Def(_) if (d.name.value == "afterAll" || d.name.value == "beforeAll") =>
+        d.mods.find(_.is[Mod.Protected]) match {
+          case Some(value) => removeWithRedundantSpace(value.tokens.toList)
+          case None => Patch.empty
+        }
       case t@Term.ApplyInfix(leftTerm, Term.Name("shouldEqual"), List(), List(rightTerm)) =>
         val newTree = Term.Apply(
           Term.Name("assertEquals"),
@@ -36,7 +42,7 @@ class ScalafixMigrateScalatestToMunit extends SemanticRule("ScalafixMigrateScala
             // remove token with `extends` and `with`
             List(
               Patch.removeGlobalImport(init.symbol),
-              Patch.removeTokens(init.tokens.toList ::: init.tokens.head.leadingKwWithOrKwExtendsWithSpaces)
+              removeWithRedundantSpace(init.tokens.toList ++ init.tokens.head.leadingKwWithOrKwExtendsWithSpaces)
             )
           case (init, Some(None)) =>
             // remove token only with `with`
@@ -45,12 +51,12 @@ class ScalafixMigrateScalatestToMunit extends SemanticRule("ScalafixMigrateScala
               case list if list.lastOption.exists(_.is[Token.KwWith]) =>
                 List(
                   Patch.removeGlobalImport(init.symbol),
-                  Patch.removeTokens(init.tokens.toList ::: leadingTokens)
+                  removeWithRedundantSpace(init.tokens.toList ++ leadingTokens)
                 )
               case _ =>
                 List(
                   Patch.removeGlobalImport(init.symbol),
-                  Patch.removeTokens(init.tokens)
+                  removeWithRedundantSpace(init.tokens.toList)
                 )
             }
           case (init, Some(Some(symbol))) =>
@@ -64,10 +70,35 @@ class ScalafixMigrateScalatestToMunit extends SemanticRule("ScalafixMigrateScala
     }.asPatch
   }
 
+  def isWhitespaceAllAround(startToken: Token, endToken: Token): Boolean =
+    startToken.is[Whitespace] && endToken.is[Whitespace]
+
+  def findRedundandWhitespaceAround(tokens: List[Token])(implicit doc: SemanticDocument): Option[Token] =
+    tokens match {
+      case Nil => None
+      case head :: Nil =>
+        for {
+          leadingToken <- doc.tokenList.leading(head).headOption
+          trailingToken <- doc.tokenList.trailing(head).headOption
+          if isWhitespaceAllAround(leadingToken, trailingToken)
+        } yield trailingToken
+      case head :: next =>
+        for {
+          leadingToken <- doc.tokenList.leading(head).headOption
+          trailingToken <- doc.tokenList.trailing(next.last).headOption
+          if isWhitespaceAllAround(leadingToken, trailingToken)
+        } yield trailingToken
+    }
+
+  def removeWithRedundantSpace(tokens: List[Token])(implicit doc: SemanticDocument): Patch = {
+    val r = findRedundandWhitespaceAround(tokens).toList
+    Patch.removeTokens(tokens ++ r)
+  }
+  
   implicit class TokenOps(token: Token) {
     def leadingKwWithOrKwExtendsWithSpaces(implicit doc: SemanticDocument): List[Token] =
       doc.tokenList.leading(token).toList
-        .span(t => t.is[Token.Space] || t.is[Token.LF]) match {
+        .span(t => t.is[Whitespace]) match {
         case (spaces, rest) => rest.headOption match {
           case Some(t) if t.is[Token.KwWith] || t.is[Token.KwExtends] =>
             spaces :+ t
